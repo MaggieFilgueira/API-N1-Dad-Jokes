7cc8c050c479e69f87b819bf5a1d84fca68af35d
Magdalena Filgueira <132447020+MaggieFilgueira@users.noreply.github.com>
2023-06-14 20:05:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,20 +10,6 @@ async function generateJoke() {
     },
 }
 
-  const res = await ('', config)
-
-  const data = await res.json()
-
-  jokeEl.innerHTML = data.joke
-}
-
-function generateJoke() {
-   const config = {
-     headers: {
-       Accept: 'application/json',
-     },
-}
-
     fetch('https://icanhazdadjoke.com', config)
      .then((res) => res.json())
      .then((data) => {
